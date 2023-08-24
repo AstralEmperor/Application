accb394a6fbf73afeab08cec6d0534e3cf001281
AstralEmperor <96700405+AstralEmperor@users.noreply.github.com>
2023-08-24 17:27:50 +0200

    statistics v3

--- FrontEnd/pages/statistics/statistic.js
+++ FrontEnd/pages/statistics/statistic.js
@@ -2,16 +2,13 @@ import { requests } from "../../../BackEnd/api-folder/api.mjs";
 import { getKorisnici } from "../../../BackEnd/api-folder/api.mjs";
 import { getProductStats } from "../../../BackEnd/api-folder/api.mjs";
 
-const statsContainer = document.querySelector('.statistics__stats-container');
    // async funkcija za pozivanje asinhrone date
 async function getStats(){
     const bKorisnika = document.getElementById('bKorisnika');
     const bOpservacija = document.getElementById('bOpservacija');
-    const bProizvoda = document.getElementById('bProizvoda');
     const bProdato = document.getElementById('bProdato');
     const bDostavljeno = document.getElementById('bDostavljeno');
     const bTransport = document.getElementById('bTransport');
-    const bCekanje = document.getElementById('bCekanje');
 
     try{
         const zahteviData = await requests()
@@ -20,24 +17,67 @@ async function getStats(){
         
         let korisnikNumber = korisnici.length;
         let brojZahteva = zahteviData.length;
-        let brojProizvoda = products.currentItems;
         let prodato = products.sold;
         let dostavljeno = products.delivered;
         let transport = products.transporting;
-        let cekanje = products.pending;
 
 
         // ovaj deo ispisuje
         bKorisnika.innerText = korisnikNumber;
         bOpservacija.innerText = brojZahteva; 
-        bProizvoda.innerText = brojProizvoda; 
         bProdato.innerText = prodato; 
         bDostavljeno.innerText = dostavljeno; 
         bTransport.innerText = transport;
-        bCekanje.innerText = cekanje;
 
     }catch(error){
         console.log(error);
     }
 }
 getStats();
+
+// done with help from https://www.chartjs3.com/docs/chart/getting-started/
+const data = {
+    labels:['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Avg'],
+    datasets:[{
+        label: 'Monthly Sales',
+        data: [22, 122, 232 ,405, 334, 502, 496, 643],
+        backgroundColor: [ 
+            'rgba(255, 26, 104, 0.4)',
+            'rgba(54, 162, 235, 0.4)',
+            'rgba(255, 206, 86, 0.4)',
+            'rgba(75, 192, 192, 0.4)',
+            'rgba(153, 102, 255, 0.4)',
+            'rgba(255, 159, 64, 0.4)',
+            'rgba(255, 59, 44, 0.4)',
+            'rgba(0, 0, 0, 0.4)'
+        ],
+        borderColor: [
+            'rgba(255, 26, 104, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255, 59, 44, 1)',
+            'rgba(0, 0, 0, 1)'
+          ],
+          borderWidth: 1
+    }]
+};
+
+const config = {
+    type: 'bar',
+    data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+}
+//render block
+ const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+ );
